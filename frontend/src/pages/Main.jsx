f052fdab6f54d91483a412c345e2011586916b70
Gal919 <galraidi00@gmail.com>
2023-04-15 12:17:29 +0300

    Done

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Header from "../components/trivia/Header";
+import Header from "../components/UI/Header";
 import List from "../components/trivia/List";
 
 const Main = () => {
   return (
     <div>
-      <Header />
+      <Header text="Trivia App" />
       <List />
     </div>
   );
