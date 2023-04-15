08c29a53d7b75aeecf750ecbb34a6565be58acbc
Gal919 <galraidi00@gmail.com>
2023-04-15 12:15:39 +0300

    add login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,34 +1,53 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { getTriviaData } from "../redux/triviaDataReducer";
+import Header from "../components/UI/Header";
+import { Button } from "../styles/Button";
+import * as S from "../styles/Login";
 
 const Login = () => {
-  const [name, setName] = useState("");
-
-  const dispatch = useDispatch();
+  const [userInfo, setUserInfo] = useState({ username: "", password: "" });
 
   const handleChange = (e) => {
     e.preventDefault();
-    setName(e.target.value);
-  };
 
-  const handleStartClick = () => {
-    //dispatch(getTriviaData());
+    const { name, value } = e.target;
+    setUserInfo((prev) => ({ ...prev, [name]: value }));
   };
 
   return (
-    <div>
-      <input
-        onChange={handleChange}
-        placeholder="enter name"
-        type="text"
-        value={name}
-      />
-      <Link onClick={handleStartClick} to={"main"}>
-        Start
-      </Link>
-    </div>
+    <S.Container>
+      <S.LeftContainer>
+        <h1>Welcome To</h1>
+        <h1>
+          Trivia App
+          <i className="fa-solid fa-medal"></i>
+        </h1>
+      </S.LeftContainer>
+      <S.RightContainer>
+        <Header text="Login" />
+        <S.LoginForm>
+          <S.LoginLabel>Username</S.LoginLabel>
+          <S.LoginInput
+            type="text"
+            name="username"
+            placeholder="Please enter username"
+            value={userInfo.username}
+            onChange={handleChange}
+          />
+          <S.LoginLabel>Password</S.LoginLabel>
+          <S.LoginInput
+            type="password"
+            name="password"
+            placeholder="Please enter password"
+            value={userInfo.password}
+            onChange={handleChange}
+          />
+        </S.LoginForm>
+        <Link to={"main"} style={S.LoginLink}>
+          <Button>Start</Button>
+        </Link>
+      </S.RightContainer>
+    </S.Container>
   );
 };
 
