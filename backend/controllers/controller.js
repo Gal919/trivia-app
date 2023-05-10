8b8cd31ac9acd779c5205023e5d30e13b84e9163
Gal919 <galraidi00@gmail.com>
2023-05-10 11:38:33 +0300

    add user login and user data controller

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcryptjs");
-const User = require("../models/userInfoModel");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+const generateToken = require("../utils/generateToken");
 
 //register controller
 const insertUser = async (req, res) => {
@@ -13,15 +15,53 @@ const insertUser = async (req, res) => {
       return res.json({ error: "User already exist" });
     }
 
-    await User.create({
+    const user = await User.create({
       username,
       email,
       password: encryptedPassword,
     });
-    res.send({ status: "ok" });
+
+    const token = generateToken(user);
+
+    res.send({ status: "ok", token: token });
   } catch (error) {
     res.send({ status: "error" });
   }
 };
 
-module.exports = { insertUser };
+//login controller
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.json({ error: "User Not Found" });
+  }
+  if (await bcrypt.compare(password, user.password)) {
+    const token = generateToken(user);
+
+    if (res.status(201)) {
+      return res.json({ status: "ok", token: token });
+    } else {
+      return res.json({ error: "error" });
+    }
+  }
+
+  return res.json({ status: "error", error: "Invalid Password" });
+};
+
+//user data controller
+const userData = async (req, res) => {
+  const { token } = req.body;
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const useremail = user.email;
+    const userData = await User.findOne({ email: useremail });
+    res.send({ status: "ok", data: userData });
+  } catch (error) {
+    res.send({ status: "error", data: error });
+  }
+};
+
+module.exports = { insertUser, userLogin, userData };
