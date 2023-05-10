f29a7ca01f156b653e334a2db86a7aace186b609
Gal919 <galraidi00@gmail.com>
2023-05-10 17:04:03 +0300

    add error handling

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,54 +1,100 @@
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import Header from "../UI/Header";
-// import { Button } from "../../styles/Button";
-// import * as S from "../../styles/Form";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import * as S from "../../styles/Form";
+import Button from "../UI/Button";
+import Header from "../UI/Header";
 
-// const Login = () => {
-//   const [userInfo, setUserInfo] = useState({ email: "", password: "" });
+const Login = ({ setIsSignupPage }) => {
+  const [userInfo, setUserInfo] = useState({ email: "", password: "" });
+  const [loginError, setLoginError] = useState("");
+  const [formError, setFormError] = useState({});
+  const [errorPost, sendPostRequest] = useFetch();
+  const navigate = useNavigate();
 
-//   const handleChange = (e) => {
-//     e.preventDefault();
+  const handleChange = (e) => {
+    e.preventDefault();
 
-//     const { name, value } = e.target;
-//     setUserInfo((prev) => ({ ...prev, [name]: value }));
-//   };
+    const { name, value } = e.target;
+    setUserInfo((prev) => ({ ...prev, [name]: value }));
+  };
 
-//   return (
-//     <S.Container>
-//       <S.LeftContainer>
-//         <h1>Welcome To</h1>
-//         <h1>
-//           Trivia App
-//           <i className="fa-solid fa-medal"></i>
-//         </h1>
-//       </S.LeftContainer>
-//       <S.RightContainer>
-//         <Header text="Login" />
-//         <S.LoginForm>
-//           <S.LoginLabel>email</S.LoginLabel>
-//           <S.LoginInput
-//             type="text"
-//             name="email"
-//             placeholder="Please enter email"
-//             value={userInfo.email}
-//             onChange={handleChange}
-//           />
-//           <S.LoginLabel>Password</S.LoginLabel>
-//           <S.LoginInput
-//             type="password"
-//             name="password"
-//             placeholder="Please enter password"
-//             value={userInfo.password}
-//             onChange={handleChange}
-//           />
-//         </S.LoginForm>
-//         <Link to={"main"} style={S.LoginLink}>
-//           <Button>Start</Button>
-//         </Link>
-//       </S.RightContainer>
-//     </S.Container>
-//   );
-// };
+  const validateForm = () => {
+    let err = {};
 
-// export default Login;
+    if (userInfo.email.trim() === "") {
+      err.email = "Please enter email";
+    }
+
+    if (userInfo.password.trim() === "") {
+      err.password = "Please enter password";
+    }
+
+    setFormError({ ...err });
+
+    return Object.keys(err).length < 1;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let isValid = validateForm();
+
+    if (isValid) {
+      sendPostRequest(
+        "http://localhost:5000/api/login",
+        "POST",
+        userInfo,
+        saveUserData
+      );
+    }
+  };
+
+  const handleLogin = () => {
+    setIsSignupPage(true);
+  };
+
+  const saveUserData = (data) => {
+    if (data.status === "ok") {
+      window.localStorage.setItem("token", data.token);
+      navigate("/trivia");
+    } else {
+      setLoginError(data.error);
+    }
+  };
+
+  return (
+    <>
+      <Header text="Login" />
+      <S.Form id="loginForm" onSubmit={handleSubmit}>
+        <S.Label>email</S.Label>
+        <S.Input
+          type="text"
+          name="email"
+          placeholder="Please enter email"
+          value={userInfo.email}
+          onChange={handleChange}
+          autoComplete="on"
+        />
+        <span>{formError.email}</span>
+        <S.Label>Password</S.Label>
+        <S.Input
+          type="password"
+          name="password"
+          placeholder="Please enter password"
+          value={userInfo.password}
+          onChange={handleChange}
+          autoComplete="on"
+        />
+        <span>{formError.password}</span>
+      </S.Form>
+      <p>Don't have an account?</p>
+      {loginError.length > 0 && <p>{loginError}</p>}
+      <button onClick={handleLogin}>Sign Up</button>
+      <Button form="loginForm" style={S.Button}>
+        Start
+      </Button>
+    </>
+  );
+};
+
+export default Login;
--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,18 +1,24 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import Header from "../UI/Header";
-import { Button } from "../../styles/Button";
-import * as S from "../../styles/Form";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
+import * as S from "../../styles/Form";
+import Button from "../UI/Button";
+import Header from "../UI/Header";
 
-const Signup = () => {
+const Signup = ({ setIsSignupPage }) => {
   const [userInfo, setUserInfo] = useState({
     username: "",
     email: "",
     password: "",
   });
-
+  const [signupError, setSignupError] = useState("");
+  const [formError, setFormError] = useState({});
   const [errorPost, sendPostRequest] = useFetch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setSignupError("");
+  }, [userInfo.email]);
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -21,10 +27,50 @@ const Signup = () => {
     setUserInfo((prev) => ({ ...prev, [name]: value }));
   };
 
+  const onCreateUser = (data) => {
+    if (data.status === "ok") {
+      navigate("/trivia");
+    } else {
+      setSignupError(data.error);
+    }
+  };
+
+  const validateForm = () => {
+    let err = {};
+
+    if (userInfo.username.trim() === "") {
+      err.username = "Please enter username";
+    }
+
+    if (userInfo.email.trim() === "") {
+      err.email = "Please enter email";
+    }
+
+    if (userInfo.password.trim() === "") {
+      err.password = "Please enter password";
+    }
+
+    setFormError({ ...err });
+
+    return Object.keys(err).length < 1;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    let isValid = validateForm();
 
-    sendPostRequest("http://localhost:5000/api/register", "POST", userInfo);
+    if (isValid) {
+      sendPostRequest(
+        "http://localhost:5000/api/register",
+        "POST",
+        userInfo,
+        onCreateUser
+      );
+    }
+  };
+
+  const handleLogin = () => {
+    setIsSignupPage(false);
   };
 
   return (
@@ -38,7 +84,9 @@ const Signup = () => {
           placeholder="Please enter username"
           value={userInfo.username}
           onChange={handleChange}
+          autoComplete="on"
         />
+        <span>{formError.username}</span>
         <S.Label>Email</S.Label>
         <S.Input
           type="email"
@@ -46,7 +94,9 @@ const Signup = () => {
           placeholder="Please enter email"
           value={userInfo.email}
           onChange={handleChange}
+          autoComplete="on"
         />
+        <span>{formError.email}</span>
         <S.Label>Password</S.Label>
         <S.Input
           type="password"
@@ -54,13 +104,16 @@ const Signup = () => {
           placeholder="Please enter password"
           value={userInfo.password}
           onChange={handleChange}
+          autoComplete="on"
         />
+        <span>{formError.password}</span>
       </S.Form>
-      {/* <Link to={"main"} style={S.Link}> */}
+      {signupError.length > 0 && <p>{signupError}</p>}
+      <p>Have an account? </p>
+      <button onClick={handleLogin}>Login now</button>
       <Button form="singUpForm" type="submit">
-        Sign Up{" "}
+        Sign Up
       </Button>
-      {/* </Link> */}
     </>
   );
 };
