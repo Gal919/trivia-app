e22aa32ef2bd91651485e77c74891796064dfc2d
Gal919 <galraidi00@gmail.com>
2023-05-10 17:02:01 +0300

    update button ui component

--- frontend/src/components/UI/Button.jsx
+++ frontend/src/components/UI/Button.jsx
@@ -1,11 +1,13 @@
 import * as S from "../../styles/Button";
 
-const Button = ({ className, onClick, children }) => {
+const Button = ({ className, onClick, form, type, children }) => {
   return (
     <S.Container>
       <S.Button
         className={className ? `btn ${className}` : "btn"}
         onClick={onClick}
+        form={form}
+        type={type}
       >
         {children}
       </S.Button>
