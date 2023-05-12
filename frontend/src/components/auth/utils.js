bcaff66493730b5488714039d17e42ab68faa34d
Gal919 <galraidi00@gmail.com>
2023-05-12 20:55:40 +0300

    fix issue

--- frontend/src/components/auth/utils.js
+++ frontend/src/components/auth/utils.js
@@ -1,15 +1,15 @@
 export const validateForm = (data, setter) => {
   let err = {};
 
-  if (data.name.trim() === "") {
+  if (data?.name?.trim() === "") {
     err.name = "Please enter username";
   }
 
-  if (data.email.trim() === "") {
+  if (data?.email?.trim() === "") {
     err.email = "Please enter email";
   }
 
-  if (data.password.trim() === "") {
+  if (data?.password?.trim() === "") {
     err.password = "Please enter password";
   }
 
