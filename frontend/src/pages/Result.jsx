bfa167ee85a7e3b940bb231c1e80dbbb359c9a7c
Gal919 <galraidi00@gmail.com>
2023-05-28 10:02:00 +0300

    add result component

--- frontend/src/pages/Result.jsx
+++ frontend/src/pages/Result.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserResult } from "../redux/ResultReducer";
+import { getResultList } from "../redux/ResultListReducer";
+import { createId } from "../components/trivia/utils";
+import { loadUser } from "../redux/authReducer";
 
 const Result = () => {
-  return <div>Result</div>;
+  const { email } = useSelector((state) => state.auth.data);
+  const { resultStatus, score } = useSelector((state) => state.result);
+  const { resultListStatus, resultList } = useSelector(
+    (state) => state.resultList
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    dispatch(loadUser(token));
+
+    if (email) {
+      dispatch(getUserResult(email));
+      dispatch(getResultList());
+    }
+  }, [dispatch, email]);
+
+  return (
+    <div>
+      {resultStatus === "pending" ? (
+        <p>loading</p>
+      ) : (
+        resultStatus === "success" &&
+        resultListStatus === "success" && (
+          <>
+            <p>Yoyr Score is: {score}</p>
+            {resultList?.map((result) => (
+              <div key={createId()}>
+                <p>{result.name}</p>
+                <p>{result.highestScore}</p>
+              </div>
+            ))}
+          </>
+        )
+      )}
+    </div>
+  );
 };
 
 export default Result;
