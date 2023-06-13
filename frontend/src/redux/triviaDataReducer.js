9414fe3f559f5d09b3d59ec079834217e0a1c597
Gal919 <galraidi00@gmail.com>
2023-06-13 10:26:56 +0300

    add trivia categories

--- frontend/src/redux/triviaDataReducer.js
+++ frontend/src/redux/triviaDataReducer.js
@@ -4,18 +4,39 @@ import axios from "axios";
 const initialState = {
   loading: false,
   data: [],
+  categories: [],
   error: false,
-  success: false,
+  dataSuccess: false,
+  categorySuccess: false,
   message: "",
   index: 0,
+  category: "",
 };
 
 export const getTriviaData = createAsyncThunk(
   "triviaData/requestStatus",
+  async (_, { rejectWithValue, getState }) => {
+    const state = getState();
+    try {
+      const response = await axios.get(
+        `https://the-trivia-api.com/api/questions?categories=${state.triviaData.category}&limit=5&difficulty=medium`
+      );
+
+      return response.data;
+    } catch (err) {
+      const message = err.response.data || err.message;
+
+      return rejectWithValue(message);
+    }
+  }
+);
+
+export const getTriviaCategories = createAsyncThunk(
+  "triviaCategories",
   async (_, thunkAPI) => {
     try {
       const response = await axios.get(
-        "https://the-trivia-api.com/api/questions?categories=general_knowledge&limit=5&difficulty=medium"
+        "https://the-trivia-api.com/api/categories"
       );
 
       return response.data;
@@ -43,18 +64,41 @@ export const triviaDataReducer = createSlice({
         index: state.index + 1,
       };
     },
+    updateCategory: (state, action) => {
+      return {
+        ...state,
+        category: action.payload,
+      };
+    },
   },
   extraReducers: (builder) => {
     builder
+      //trivia data
       .addCase(getTriviaData.pending, (state) => {
         state.loading = true;
       })
       .addCase(getTriviaData.fulfilled, (state, action) => {
         state.loading = false;
         state.data = action.payload;
-        state.success = true;
+        state.dataSuccess = true;
       })
       .addCase(getTriviaData.rejected, (state, action) => {
+        state.loading = false;
+        state.error = true;
+        state.message = action.payload;
+        state.data = [];
+      })
+
+      //trivia categories
+      .addCase(getTriviaCategories.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getTriviaCategories.fulfilled, (state, action) => {
+        state.loading = false;
+        state.categories = action.payload;
+        state.categorySuccess = true;
+      })
+      .addCase(getTriviaCategories.rejected, (state, action) => {
         state.loading = false;
         state.error = true;
         state.message = action.payload;
@@ -63,5 +107,5 @@ export const triviaDataReducer = createSlice({
   },
 });
 
-export const { updateIndex } = triviaDataReducer.actions;
+export const { updateIndex, updateCategory } = triviaDataReducer.actions;
 export default triviaDataReducer.reducer;
